dba5c8a687cf149ad4fedf2eeb72ee86d54ec36d
Kossaby-Dev <yassine.kossaby.work@gmail.com>
2022-12-08 21:02:18 +0100

    Add load event

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,40 @@
 var BookingForm = document.querySelectorAll(".vp_booking-form");
-
+var btnBookingDesktop = document.querySelector("button.vp_btn-booking");
+var btnBookingMobile = document.querySelector("a.vp_btn-booking");
+var btnGuideDesktop = document.querySelectorAll(".vp_guide-desktop");
+var btnGuideMobile = document.querySelectorAll(".vp_guide-mobile");
 
 BookingForm.forEach(el => {
     el.addEventListener("submit",function(e){
         e.preventDefault();
     })
 })
+
+function switchBtn(){
+    if (window.innerWidth > 428) {
+        btnBookingDesktop.style.display = "block";
+        btnBookingMobile.style.display = "none";
+        btnGuideDesktop.forEach(el => {
+          el.style.display = "block"
+        });
+        btnGuideMobile.forEach(el => {
+            el.style.display = "none"
+          });
+    } else {
+        btnBookingDesktop.style.display = "none";
+        btnBookingMobile.style.display = "block";
+        btnGuideDesktop.forEach(el => {
+            el.style.display = "none"
+          });
+          btnGuideMobile.forEach(el => {
+              el.style.display = "block"
+            });
+    }
+}
+
+;
+
+document.addEventListener("DOMContentLoaded", switchBtn);
+window.addEventListener('resize', switchBtn)
 
 
